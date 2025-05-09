ff36fd2db1b292c9dbf54bac3963219d212fa4f0
ViniciusGomes <viniciuslucasvlcg@gmail.com>
2025-05-09 12:59:17 -0300

    Criação do banco de dados

--- projetoCadastroUsuario/server.js
+++ projetoCadastroUsuario/server.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const sqlite3 = require('sqlite3')
+const db = new sqlite3.Database('./db.sqlite')
 
-// app.get('/search',)
-// app.post('/register',)
+function saveUser() {
+  db.serialize(() => {
+    db.run('CREATE TABLE IF NOT EXISTS teste(username TEXT, email TEXT, password TEXT)')
+  
+  db.run("INSERT INTO teste (username, email, password) VALUES (?, ?, ?)", ['USERNAME', 'EMAIL', 'PASSWORD'])
+  })
+}
+saveUser()
 
-app.listen(3030, () => {
-  console.log('Executando...')
-})
+// app.use(cors())
+// app.use('/teste', (req, res) => {
+//   res.send('teste')
+// })
+
+// app.listen(3030, () => {
+//   console.log('Executando...')
+// })
