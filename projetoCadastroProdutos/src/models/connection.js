2825cad1abca100350971e7b2168bf2d6ccdeb57
ViniciusGomes <viniciuslucasvlcg@gmail.com>
2025-05-19 14:44:16 -0300

    Config Banco de dados sqlite3

---
+++ projetoCadastroProdutos/src/models/connection.js
@@ -0,0 +1,19 @@
+const sqlite3 = require('sqlite3').verbose()
+const path = require('path')
+
+const db = new sqlite3.Database(path.join(__dirname, '../..', 'db.sqlite3'))
+
+db.serialize(() => {
+  db.run(`CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    sobrenome TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    senha TEXT NOT NULL)`)
+})
+
+function insertData() {
+
+}
+
+db.close()
