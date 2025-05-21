f6b436c4ee03ffbc8a4a1158002cbf63626acd0f
ViniciusGomes <viniciuslucasvlcg@gmail.com>
2025-05-20 21:03:05 -0300

    CRUD do user

--- projetoCadastroProdutos/src/models/userModel.js
+++ projetoCadastroProdutos/src/models/userModel.js
@@ -3,16 +3,40 @@ const db = require('./connection')
 const saveUser = async (req, res, next) => {
   const query = `INSERT INTO users(nome, sobrenome, email, senha) VALUES (?, ?, ?, ?)`
   const { nome, sobrenome, email, senhaHash } = req.body
-  
+
   db.run(query, [nome, sobrenome, email, senhaHash])
   next()
 }
 
-const deleteUser = () => {}
+const deleteUser = (req, res, next) => {
+  const query = `DELETE FROM users WHERE id = (?)`
+  const id = req.body.id
 
-const UpdateUser = () => {}
+  db.run(query, id)
+  next()
+}
 
-const showUser = () => {}
+const UpdateUser = (req, res, next) => {
+  const query = `UPDATE users SET name = (?), sobrenome = (?), email = (?)`
+
+  const { nome, sobrenome, email} = req.body
+
+  db.run(query, [nome, sobrenome, email])
+  next()
+}
+
+const showUser = (req, res, next) => {
+  if(req.body.id) {
+    const query = `SELECT * FROM users WHERE id = (?)`
+    
+    db.run(query, id)
+    next()
+  }
+  const query = `SELECT * FROM users`
+
+  db.run(query)
+  next()
+}
 
 module.exports = {
   saveUser,
